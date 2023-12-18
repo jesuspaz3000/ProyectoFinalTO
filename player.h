b9887eb34b712420b84edc39efda862ea99e35c1
Jesus <64039758+jesuspaz3000@users.noreply.github.com>
2023-12-17 21:24:59 -0500

    error de codigo

--- player.h
+++ player.h
@@ -1,43 +1,5 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <QObject>
-#include <QRect>
-#include <QTimer>
-#include <QPixmap>
-
-class Player : public QObject {
-    Q_OBJECT
-public:
-    Player(QObject *parent, int x, int y);
-
-    void moveLeft();
-    void moveRight();
-    void moveUp();
-    void moveDown();
-    void stopLeft();
-    void stopRight();
-    void stopUp();
-    void stopDown();
-
-    void setRect(const QRect &newRect);
-
-    QRect getRect() const;
-    QPixmap getPixmap() const;
-
-signals:
-    void positionChanged();
-
-private:
-    QRect rect;
-    QPixmap pixmap;
-    int speed;
-    QTimer *timer;
-    bool movingLeft, movingRight, movingUp, movingDown;
-    int dx, dy;
-
-private slots:
-    void updatePosition();
-};
 
 #endif // PLAYER_H
