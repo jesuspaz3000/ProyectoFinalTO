0f7848f01e46de62bf7d5623df807933250715aa
milagrosparicanaza <mparicanazac@unsa.edu.pe>
2023-12-17 21:50:56 -0500

    Creación constructor GameObject
    creamos un constructor que inicializará un objeto llamado GameObject con diversas propiedades como: posición, forma, tamaño, y otros atributos.

--- gameobject.cpp
+++ gameobject.cpp
@@ -1,6 +1,10 @@
 #include "gameobject.h"
+#include <QWidget>
+#include <QRandomGenerator>
 
-GameObject::GameObject()
-{
-
+// Constructor: Inicializa un GameObject
+GameObject::GameObject(int x, int y, int size, Shape shape, QColor color, QObject *parent)
+    : QObject(parent), rect(x, y, size, size), shape(shape), color(color),
+    movingLeft(QRandomGenerator::global()->bounded(2)), speed(2) {
+    parentWidget = qobject_cast<QWidget*>(parent);  // Convierte el objeto padre a QWidget
 }
