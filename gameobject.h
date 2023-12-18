ad7d7c353d068555f7d1e9f26ad849715012848c
milagrosparicanaza <mparicanazac@unsa.edu.pe>
2023-12-17 22:39:56 -0500

    Creación de Herencia pública
    GameOBject hereda de QObject, que es una clase base Qt que proporciona funcionalidades básicas para trabajar con señales y ranuras, manejo de eventos, etc.
    
    Así mismo hacemos uso de la presencia de Q_OBJECT indica que la clase utiliza características específicas de Qt que requieren la generación de información adicional durante la compilación.

--- gameobject.h
+++ gameobject.h
@@ -1,9 +1,14 @@
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
+#include <QObject>
+#include <QRect>
+#include <QPainter>
+#include <QColor>
 
-class GameObject
+class GameObject: public QObject //Herencia pública de la clase QObject
 {
+    Q_OBJECT //macro que brinda a la clase carácterísticas específicas de Qt
 public:
     GameObject();
 };
